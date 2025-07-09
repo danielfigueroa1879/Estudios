3ddd8882a7a22c5620b4f3f39e6da07aec576b15
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-07-09 11:37:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -783,7 +783,7 @@ const AcademicTaskManager = ({ user }) => {
                                 <div className="text-white"><IconBook width="26" height="26" /></div>
                                 <div>
                                     <h1 className="text-lg sm:text-2xl font-bold text-white leading-tight">GESTOR ACADÉMICO</h1>
-                                    <p className="text-[14px] text-blue-200 mt-1"><span className="font-semibold text-white">{user.email}</span></p>
+                                    <p className="text-[08px] text-blue-200 mt-1"><span className="font-semibold text-white">{user.email}</span></p>
                                 </div>
                             </div>
                             <div className="flex-shrink-0 flex items-center space-x-1 sm:space-x-2">
