7095dba668e6a4ee5f6c8e1a0381b87de6bbb0fe
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-08-16 05:24:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1296,7 +1296,7 @@ const ClassModal = ({
 };
 
 // Define time slots globally for easier modification
-const WEEKLY_CALENDAR_TIME_SLOTS = ['06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '19:00', '20:00', '22:00' // 9 rows
+const WEEKLY_CALENDAR_TIME_SLOTS = ['06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '19:00', '20:00', '22:00'
 ];
 
 // --- NEW: Weekly Calendar View Component ---
